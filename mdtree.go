19fe42d55e5f1b8927e69b96b83c3d352e513488
Katsuhiro OGAWA <ko.fivestar@gmail.com>
2014-09-08 19:34:20 +0900

    Replace with golang

---
+++ mdtree.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "math"
+    "os"
+    "os/exec"
+    "regexp"
+    "strings"
+)
+
+func main() {
+    args := os.Args[1:]
+
+    hasColorArg := false
+    for _, arg := range args {
+        if arg == "-C" || arg == "-n" {
+            hasColorArg = true
+            break
+        }
+    }
+    if !hasColorArg {
+        args = append(args, "-C")
+    }
+
+    out, err := exec.Command("tree", args...).Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    currentPos := -1
+    indentLv := 0
+
+    lines := strings.Split(string(out[:]), "\n")
+    for _, line := range lines {
+        if line == "" {
+            fmt.Println()
+            continue
+        }
+
+        pos := -1
+
+        needles := []string{"│", "├", "└"}
+        for _, needle := range needles {
+            p := strings.LastIndex(line, needle)
+            if p > -1 {
+                pos = int(math.Max(float64(p), float64(pos)))
+            }
+        }
+
+        switch {
+        case pos == -1:
+            indentLv = 0
+            break
+        case pos == 0:
+            indentLv = 1
+            break
+        case currentPos < pos:
+            indentLv++
+            break
+        case currentPos > pos:
+            indentLv--
+            break
+        }
+
+        currentPos = pos
+
+        name := ""
+        re := regexp.MustCompile("── (.+)$")
+        matches := re.FindStringSubmatch(line)
+        if len(matches) > 0 {
+            name = matches[1]
+        } else {
+            name = line
+        }
+
+        fmt.Println(strings.Repeat("    ", indentLv) + "* " + name)
+    }
+}
